13a15c554fd117a02b3b6d2f4e65d0cd66a85d62
Warren Toomey <wkt@eccles.local.net>
2026-01-14 11:38:11 +1000

    blinky.v now compiles with Verilator 5.

--- blinky.v
+++ blinky.v
@@ -2,9 +2,6 @@
 /* verilator lint_off UNUSED */		// Not all of btn gets used, sigh
 module blinky(input i_clk, input [6:0] btn, output [7:0] o_led);
 /* verilator lint_on UNUSED */
-    wire i_clk;
-    wire [6:0] btn;
-    wire [7:0] o_led;
 `else
 module top(input clk_25mhz,
            input [6:0] btn,
@@ -21,7 +18,9 @@ module top(input clk_25mhz,
 `endif
 
     localparam ctr_width = 32;
+/* verilator lint_off PROCASSINIT */
     reg [ctr_width-1:0] ctr = 0;
+/* verilator lint_on PROCASSINIT */
 
     always @(posedge i_clk) begin
                ctr <= ctr + 1;
